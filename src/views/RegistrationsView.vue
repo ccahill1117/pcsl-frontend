32fd9317226a4e8790084cb77bcad9d356acd756
ccahill <christopher.cahill@gartner.com>
2023-03-21 15:47:24 -0700

    add api utils for axios

--- src/views/RegistrationsView.vue
+++ src/views/RegistrationsView.vue
@@ -2,17 +2,26 @@
   <div class="about">
     <h1>Here we can see the registrations as they come in</h1>
     <RegistrationsIndex />
+    <!-- <p>{{ ApiUtilities }}</p> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import RegistrationsIndex from '@/components/RegistrationsIndex.vue'
+import { ApiUtilities } from '../../utils/ApiUtilities.js'
 
 export default {
   name: 'RegistrationsView',
   components: {
     RegistrationsIndex
+  },
+  methods () {
+    // methods
+  },
+  mounted () {
+    console.log('apiUtilities', ApiUtilities)
+    console.log('reg index mounted')
   }
 }
 </script>
---
+++ utils/ApiUtilities.js
@@ -0,0 +1,8 @@
+// const baseUrl = () => {
+//   return 'baseUrl'
+// }
+
+export const ApiUtilities = {
+  // baseUrl: baseUrl()
+  utils: 'utils'
+}
