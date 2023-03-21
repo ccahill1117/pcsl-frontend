9e245d8730e670833cb6ae1e5c669d8542056428
ccahill <christopher.cahill@gartner.com>
2023-03-21 15:11:09 -0700

    reg index view

---
+++ src/components/RegistrationsIndex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hi">
+    <p>
+      Let's put table of registrations here as they come in
+    </p>
+    <p>drag and drop would be nice too</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegistrationsIndex',
+  props: {
+    registrations: Array,
+    someProp: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- src/views/RegistrationsView.vue
+++ src/views/RegistrationsView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>Here we can see the registrations as they come in</h1>
+    <RegistrationsIndex />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import RegistrationsIndex from '@/components/RegistrationsIndex.vue'
+
+export default {
+  name: 'RegistrationsView',
+  components: {
+    RegistrationsIndex
+  }
+}
+</script>
