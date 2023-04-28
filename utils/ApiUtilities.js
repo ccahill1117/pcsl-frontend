d8c9a7e93692198f3db3599c4c5271eaac98f43b
Christopher Cahill <chris@christophercahill.co>
2023-04-27 20:20:30 -0700

    push up

--- utils/ApiUtilities.js
+++ utils/ApiUtilities.js
@@ -2,7 +2,13 @@
 //   return 'baseUrl'
 // }
 
+// import axios from 'axios'
+
 export const ApiUtilities = {
-  // baseUrl: baseUrl()
+  baseUrl: process.env.VUE_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    withCredentials: true
+  },
   utils: 'utils'
 }
