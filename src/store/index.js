794b1a68371251e23ec31483f5ee575000234c73
Christopher Cahill <chris@christophercahill.co>
2023-05-08 15:35:26 -0700

    adding state

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: null,
+    token: null
   },
   getters: {
+    isLoggedIn (state) {
+      return !!state.token
+    }
   },
   mutations: {
+    setUser (state, user) {
+      state.user = user
+    },
+    setToken (state, token) {
+      state.token = token
+    }
   },
   actions: {
   },
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="login">
     <h1>Login</h1>
-    <!-- <RegistrationsIndex /> -->
     <div>
       <label>email</label>
       <input v-model="email">
@@ -14,11 +13,15 @@
       <button @click="login">Login</button>
     </div>
   </div>
+  <div class="current-user">
+    <button @click="getCurrentUser">get current user</button>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import axios from 'axios'
+import { mapMutations } from 'vuex'
 // import RegistrationsIndex from '@/components/RegistrationsIndex.vue'
 // import { ApiUtilities } from '../../utils/ApiUtilities.js'
 
@@ -34,14 +37,34 @@ export default {
     }
   },
   methods: {
-    login () {
+    ...mapMutations(['setUser', 'setToken']),
+    async login () {
       console.log(this.email, this.password)
       const user = { user: { email: this.email, password: this.password } }
-      axios.post(process.env.VUE_APP_API_URL + '/login', user)
-        .then(response => console.log('resp', response))
+      // const config = { withCredentials: true }
+      await axios.post(process.env.VUE_APP_API_URL + '/login', user)
+        .then(response =>
+          this.setUserAndToken(response)
+        )
+    },
+    getCurrentUser () {
+      console.log('user')
+    },
+    setUserAndToken (response) {
+      console.log('resp', response)
+      this.setUser(response.data.data.email)
+      this.setToken(response.headers.authorization)
     }
   },
   mounted () {
   }
 }
 </script>
+
+<style>
+.current-user {
+  border-width: 30;
+  padding: 30px;
+  border-color: white;
+}
+</style>
