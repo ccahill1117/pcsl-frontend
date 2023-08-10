578fd533e05bd604eb7d083854e57dd998a0a7e2
Christopher Cahill <chris@christophercahill.co>
2023-08-09 17:34:56 -0700

    mounted for userprofile

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="user-profile">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h3>user profile</h3>
+
+    <labe>firstName</labe>
+    <input type="text" v-model="firstName"/>
+
   </div>
 </template>
 
@@ -12,6 +17,42 @@ export default {
   name: 'UserProfileView',
   components: {
     // HelloWorld
+  },
+  data () {
+    return {
+      // sign up data
+      firstName: '',
+      lastName: '',
+      address1: '',
+      address2: '',
+      state: '',
+      zipCode: '',
+      email: '',
+      phone: '',
+      gender: '',
+      dateOfBirth: '',
+      usSquashId: '',
+      selected: null,
+
+      // other
+      clubs: []
+    }
+  },
+  mounted () {
+    this.getUser()
+  },
+  methods: {
+    signUp () {
+      console.log('call API', this.email)
+    },
+    async getUser () {
+      console.log('hey get da user')
+      // console.log('token', this.user.token)
+      // const key = { key: 'club' }
+      // await axios.get(process.env.VUE_APP_API_URL + '/user')
+      //   .then(resp => { this.clubs = resp.data }
+      //   )
+    }
   }
 }
 </script>
