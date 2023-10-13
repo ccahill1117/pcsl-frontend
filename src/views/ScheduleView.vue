9684022cd181b5d8e95f17600f7bdf97b2d784f9
Christopher Cahill <chris@christophercahill.co>
2023-10-13 15:55:58 -0400

    fall2023 full sched

--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -1,27 +1,242 @@
 <template>
   <div class="dev">
     <h1>Fall 2023 Schedule</h1>
-    <div class="centeredTable">
-      <div class="row"><div class="col-md-12"><div class="panel panel-success"><div class="panel-heading "><h3 class="panel-title"></h3>Week 1 - 10/11</div>
-<table border=1 class="table table-striped table-bordered table-hover table-condensed">
-<thead><tr><th title="Field #1">Div 1</th>
-<th title="Field #2">Div 2</th>
+    <table class="table table-bordered table-hover table-condensed">
+<thead><tr><th title="Field #1"><strong>Week 1 - 10/11</strong></th>
 </tr></thead>
-<tbody><tr><td>Team 1 BYE</td>
+<tbody><tr>
+<td>Div 1</td>
+<td>Div 2</td>
+</tr>
+<tr>
+<td>Team 1 BYE</td>
+<td>Team 1 BYE </td>
+</tr>
+<tr>
+<td>Team 2 vs Team Reed Reed 7:30</td>
+<td>Team 2 BYE </td>
+</tr>
+<tr>
+<td>Team 3 vs Team 6 MAC 6:30</td>
+<td>Team 3 vs Team Reed Reed 6:30</td>
+</tr>
+<tr>
+<td>Team 4 vs Team 5 MAC 6:30</td>
+<td>Team 4 vs Team 5 LAC 6:15</td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Week 2 - 10/18</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td>Div 1</td>
+<td>Div 2</td>
+</tr>
+<tr>
+<td>Team 1 vs Team 4 MAC 6:30</td>
+<td>Team 2 vs Team 5 LAC 6:15</td>
+</tr>
+<tr>
+<td>Team 3 vs Team 5 MAC 6:30</td>
+<td>Team 3 vs Team  4 LAC 7:15</td>
+</tr>
+<tr>
+<td>Team 2 vs Team 6 MAC 7:30</td>
 <td>Team 1 BYE</td>
 </tr>
-<tr><td>Team 2 Vs Reed (Reed 7:30)</td>
+<tr>
+<td>Team Reed BYE</td>
+<td>Team Reed BYE</td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Week 3 - 10/25</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td>Div 1</td>
+<td>Div 2</td>
+</tr>
+<tr>
+<td>Team 1 vs Team 6 LAC 6:15</td>
+<td>Team 1 vs Team Reed Reed 6:30</td>
+</tr>
+<tr>
+<td>Team 5 vs Team Reed Reed 7:30</td>
+<td>Team 5 BYE</td>
+</tr>
+<tr>
+<td>Team 4 BYE</td>
+<td>Team 4 BYE</td>
+</tr>
+<tr>
+<td>Team 2 vs Team 3 MAC 7:30</td>
+<td>Team 2 vs Team 3 LAC 7:15</td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>11/1 OREGON OPEN</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Week 4 - 11/8</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td>Div 1</td>
+<td>Div 2</td>
+</tr>
+<tr>
+<td>Team 1 vs Team 5 LAC 6:15</td>
+<td>Team 1 vs Team 5 MAC 6:30</td>
+</tr>
+<tr>
+<td>Team 4 vs Team 6 LAC 7:15</td>
+<td>Team 4 vs Team Reed Reed 7:30</td>
+</tr>
+<tr>
+<td>Team 3 vs Team Reed Reed 6:30</td>
+<td>Team 3 BYE</td>
+</tr>
+<tr>
 <td>Team 2 BYE</td>
+<td>Team 2 BYE </td>
 </tr>
-<tr><td>Team 3 Vs Team 6 (MAC 6:30)</td>
-<td>Team 3 Vs Reed (Reed 6:30)</td>
+<tr>
+<td> </td>
+<td> </td>
 </tr>
-<tr><td>Team 4 Vs Team 5 (Mac 6:30)</td>
-<td>Team 4 Vs Team 5 (Mac 7:30)</td>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Week 5 - 11/15</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td>Team 1 vs Team Reed Reed 6:30</td>
+<td>Team 1 vs Team 4 LAC 7:15</td>
+</tr>
+<tr>
+<td>Team 6 BYE</td>
+<td>Team 3 vs Team 5 LAC 6:15</td>
+</tr>
+<tr>
+<td>Team 2 vs Team 5 MAC 7:30</td>
+<td>Team 2 vs Team Reed Reed 7:30</td>
+</tr>
+<tr>
+<td>Team 3 vs Team 4 MAC 6:30</td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>11/22 - THANKSGIVING</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Week 6 - 11/29</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td>Div 1</td>
+<td>Div 2</td>
+</tr>
+<tr>
+<td>Team 1 vs Team 3 LAC 6:15</td>
+<td>Team 1 vs Team 3 MAC 6:30</td>
+</tr>
+<tr>
+<td>Team 2 v Team 4 LAC 6:15</td>
+<td>Team 2 vs Team 4 MAC 6:30</td>
+</tr>
+<tr>
+<td>Team 5 BYE</td>
+<td>Team 5 BYE </td>
+</tr>
+<tr>
+<td>Team 6 vs Team Reed Reed 6:30</td>
+<td>Team Reed BYE</td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Week 7 - 12/6</strong></td>
+<td> </td>
+</tr>
+<tr>
+<td>Div 1</td>
+<td>Div 2</td>
+</tr>
+<tr>
+<td>Team 1 vs Team 2 MAC 7:30</td>
+<td>Team 1 vs Team 2 LAC 6:15</td>
+</tr>
+<tr>
+<td>Team 3 BYE</td>
+<td>Team 3 BYE</td>
+</tr>
+<tr>
+<td>Team 4 vs Team Reed Reed 7:30</td>
+<td>Team 4 BYE</td>
+</tr>
+<tr>
+<td>Team 5 vs Team 6 MAC 6:30</td>
+<td>Team 5 vs Team Reed Reed 6:30</td>
 </tr>
 </tbody></table>
-</div></div></div>
-    </div>
 
   </div>
 </template>
