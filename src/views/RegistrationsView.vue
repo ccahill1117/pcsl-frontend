10a6eebcb3de2ae855e565b979fd56c43223263d
Christopher Cahill <chris@christophercahill.co>
2023-10-31 18:26:28 -0700

    working

--- src/views/RegistrationsView.vue
+++ src/views/RegistrationsView.vue
@@ -27,7 +27,7 @@ export default {
   },
   data () {
     return {
-      registrations: '',
+      registrations: [],
       um: 'hi'
     }
   },
@@ -36,9 +36,10 @@ export default {
     async getRegistrations () {
       // console.log('token', this.user.token)
       await axios.get(process.env.VUE_APP_API_URL + '/user_registrations/1')
+        // .then(resp => { console.log('hihi', resp) })
         .then(resp => { this.registrations = resp.data.data })
-        // .then(resp => console.log('resp', JSON.stringify(resp.data.data)))
-      // console.log('aa', (_.filter(this.registrations, reg => reg.attributes.regular === true)))
+      // .then(resp => console.log('resp', JSON.stringify(resp.data.data)))
+      console.log('aa', this.data.registrations)
     }
   },
   mounted () {
